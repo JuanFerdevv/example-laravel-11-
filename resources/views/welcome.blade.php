c6b435e0a3906f3365b733eee8dfe12bbd18d850
Juan Calvache <juanferdev006@gmail.com>
2024-10-06 16:20:47 -0500

    message welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,14 +5,15 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Alquileres Guaranda</title>
         <link rel="icon" type="image/x-icon" href="/favicon.ico">    
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         @vite('resources/css/app.css')
     </head>
-    <body>
-        
-        <div class="">
-        <img src="/LogoP.svg" alt="Logo alquileres Gda" class="mx-auto -my-60">
+    <body class=" bg-gradient-to-r from-red-500 to-yellow-500">
+        <img src="/logopng.png" alt="Logo alquileres Gda"  class="mx-auto my-14 ">
+        <div class="bg-white mx-auto rounded-lg w-[300px ] md:w-[800px] h-[400px] ">
+            <h1 class="antialiased font-mono text-xl">
+                ¡Estamos renovando nuestra página web para ofrecerte una experiencia más dinámica y amigable! 
+                Pronto podrás disfrutar de una interfaz mejorada. ¡Gracias por tu paciencia!
+            </h1>
         </div>
     </body>
     
