c46e161fb7ef45e297d96a33fb77d3f510011b6c
Juan Calvache <juanferdev006@gmail.com>
2024-10-06 18:06:26 -0500

    remove hot from build

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,16 +5,20 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Alquileres Guaranda</title>
         <link rel="icon" type="image/x-icon" href="/favicon.ico">    
-        @vite('resources/css/app.css')
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class=" bg-gradient-to-r from-red-500 to-yellow-500">
+    <body id="app" class=" bg-gradient-to-r from-red-500 to-yellow-500">
         <img src="/logopng.png" alt="Logo alquileres Gda"  class="mx-auto my-14 ">
-        <div class="bg-white mx-auto rounded-lg w-[300px ] md:w-[800px] h-[400px] ">
-            <h1 class="antialiased font-mono text-xl">
+        <div class="bg-white  mx-auto rounded-lg w-[300px ] md:w-[800px] h-[200px] ">
+            <div class="my-8">
+            <h1 class="antialiased tracking-tight  font-mono  text-2xl text-center grid ">
                 ¡Estamos renovando nuestra página web para ofrecerte una experiencia más dinámica y amigable! 
                 Pronto podrás disfrutar de una interfaz mejorada. ¡Gracias por tu paciencia!
             </h1>
+            </div>
         </div>
+        <h1 class="text-center text-3xl mt-4">Nueva interfaz grafica</h1>
+        @vite(['resources/js/app.js'])
     </body>
     
 </html>
